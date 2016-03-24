005c0d7bb3a2b4bfdfdc9fbf4d83a998ed469809
Cian Hatton <cianhatton@gmail.com>
2016-03-24 01:00:22 +0000

    starting css

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-
-})
+.controller('AppCtrl', function($scope) {})
 
 .controller('AllCtrl', function($scope,AllTasks) {
   $scope.data = AllTasks;
@@ -13,22 +11,35 @@ angular.module('starter.controllers', [])
   $scope.currentTask = CurrentTask;
   $scope.addButton = false; // to show the add button div
   $scope.toggleAdd = function(){ // toggle the showing
-    console.log("Click");
     $scope.addButton = !$scope.addButton;
   }
 })
 
-.controller('FinishedCtrl', function($scope) {
-
+.controller('FinishedCtrl', function($scope,AllTasks) {
+  $scope.data = AllTasks;
 })
 
 .controller('SettingsCtrl', function($scope,AllTasks,CurrentTask) {
   $scope.data = AllTasks;
   $scope.currentTask = CurrentTask;
+
+  $scope.areYouSure = false;
+  $scope.showReset = false;
+
+  $scope.toggleAreYouSure = function(){
+    $scope.areYouSure = !$scope.areYouSure;
+  }
+
   $scope.reset = function(){
+   
+
     // http://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
     while($scope.data.allTasks.length > 0){
       $scope.data.allTasks.pop();
+    } // remove every element
+
+    while($scope.data.finishedTasks.length > 0){
+      $scope.data.finishedTasks.pop();
     } // remove every element
 
     $scope.currentTask.dueDate = null;
@@ -38,6 +49,8 @@ angular.module('starter.controllers', [])
     // don't need to do anything with assigned date
     // as it gets set on addition to the list
     $scope.data.save(); // save the now reset state of the app
+  
+    $scope.areYouSure = false;
   }
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -13,17 +13,31 @@ angular.module('starter.services', [])
 			taskName:taskName,
 			dueDate:dueDate,
 			category:category,
+			finished:false, // starts off not finished
+			show:false, // starts off hidden
+			finishDate:null, // don't have a finish date to start with
 			startDate:startDate,
 		    properties:[taskDescription,
 		    "Due: " + dueDate.toDateString(),
 		    "Started: " + startDate.toDateString()]}; 
 		listToAddTo.push(newTask); // add it to all tasks too
 		save();
+		addButton = false;
+	}
+
+	var finishTask = function(index){
+		// purpose is to remove for alltasks and add to finished
+		 var itemToRemove = allTasks[index];
+		 allTasks.splice(index, 1) // remove it from the list
+		 toggleTask(itemToRemove); // so it doesn't appear opened up
+		 itemToRemove.properties.push("Finished: " + new Date().toDateString());
+		 // add the new finished property to be displayed in the finished tab
+		 finishedTasks.push(itemToRemove); // add it
+		 save(); // save changes
 	}
 
 	// http://codepen.io/shengoo/pen/bNbvdO/
   	// I got the collapsing list stuff from here
-
 	var toggleTask = function(task) {
     	task.show = !task.show;
   	};
@@ -52,16 +66,21 @@ angular.module('starter.services', [])
 		});
 	}
 
-	function save(){
-		window.localStorage['tasks'] = JSON.stringify(allTasks);
-		window.localStorage['finished'] = JSON.stringify(finishedTasks);
-	}
+	//http://stackoverflow.com/questions/18826320/what-is-the-hashkey-added-to-my-json-stringify-result
+	// I learned I needed to use angular.toJson instead of JSON.stringify to remove the $$hashcode
+	// which is created by the ng-repeat directive. It was causing some problems when trying to
+	// load from local storage, saying there were repeats.
 
+	function save(){
+		window.localStorage['tasks'] = angular.toJson(allTasks);
+		window.localStorage['finished'] = angular.toJson(finishedTasks);
+	}
 
 	return {allTasks:allTasks,addTask:addTask,
 		finishedTasks:finishedTasks, 
 		sortByDueDate:sortByDueDate,sortByDateAdded:sortByDateAdded,
-		toggleTask:toggleTask,isTaskShown:isTaskShown, save:save}
+		toggleTask:toggleTask,isTaskShown:isTaskShown, save:save,
+		finishTask:finishTask}
 })
 
 .factory('CurrentTask',function(){
